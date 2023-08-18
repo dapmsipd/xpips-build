b31fd2f9c433f80ffde7b5157bc272fd56451424
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-08-18 20:15:12 +0800

    wrong link

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "868449ac59434bbbeae39692a24af023",
-"index.html": "e6ae13c1c4fb9800725b24bee66e3291",
-"/": "e6ae13c1c4fb9800725b24bee66e3291",
+"index.html": "c3fb2fa10d3bb4c7e78ff0a0b65d6f0e",
+"/": "c3fb2fa10d3bb4c7e78ff0a0b65d6f0e",
 "firebase-messaging-sw.js": "8c17e4dc7ecc735b6b5c262615a5b605",
 "main.dart.js": "f7defd39610b5a623e91bb2f624028eb",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
