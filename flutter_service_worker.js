6dd5879d227dfe8b0f27f4792ec0358f9c6bd041
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-09-04 21:09:28 +0800

    Fixes mlab817/pips#24

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "868449ac59434bbbeae39692a24af023",
-"index.html": "d97164f04cbcef6dcb781c76da12ea22",
-"/": "d97164f04cbcef6dcb781c76da12ea22",
+"index.html": "a92155a115c93437846b19bd1a4e7ed2",
+"/": "a92155a115c93437846b19bd1a4e7ed2",
 "firebase-messaging-sw.js": "8c17e4dc7ecc735b6b5c262615a5b605",
-"main.dart.js": "121234e7c0405ddb6c13fed4399efa0d",
+"main.dart.js": "5eb73b69fc29e25b4946a5c040f123bc",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "2428af5f724dd3374de3718ba29f45a2",
 "logo.png": "987dc3aee49fe091ef33a96487d0ddba",
