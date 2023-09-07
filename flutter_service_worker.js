eee0803d72d6af407c36d210e6d7f33bc64de690
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-09-07 13:53:21 +0800

    Fixes #179

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "868449ac59434bbbeae39692a24af023",
-"index.html": "37862ef559f4e73e2e9764376e074a77",
-"/": "37862ef559f4e73e2e9764376e074a77",
+"index.html": "30bb35a3e862250432e03d0c6ecf8862",
+"/": "30bb35a3e862250432e03d0c6ecf8862",
 "firebase-messaging-sw.js": "8c17e4dc7ecc735b6b5c262615a5b605",
-"main.dart.js": "4a3f7d7ab642f0b027b8e5381149f174",
+"main.dart.js": "eb240901642f47a476a70696bfb271c1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "2428af5f724dd3374de3718ba29f45a2",
 "logo.png": "987dc3aee49fe091ef33a96487d0ddba",
