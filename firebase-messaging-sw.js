b71f9ba4bf6de7d5564d6c399c3e6b28f1e8bd2e
mlab817 <mlab817@gmail.com>
2023-09-11 23:33:56 +0000

    Update from https://github.com/mlab817/xpips/commit/9c24623071172f2293bb6ee419d100bfa228a524

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,29 +1,30 @@
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
 
-   /*Update with yours config*/
-  const firebaseConfig = {
-    apiKey: "AIzaSyCPEw_dp5Ed5NRs_MC5FKBfN0rTrtm2Vfc",
-    authDomain: "pips-demo-app.firebaseapp.com",
-    projectId: "pips-demo-app",
-    storageBucket: "pips-demo-app.appspot.com",
-    messagingSenderId: "595239640103",
-    appId: "1:595239640103:web:fed3ab7a7a6bc2a44bc202",
-    measurementId: "G-1M8YD1QNH9"
- };
-  firebase.initializeApp(firebaseConfig);
-  const messaging = firebase.messaging();
+/*Update with yours config*/
+const firebaseConfig = {
+  apiKey: "AIzaSyBgr5pdFKoX0Ekt_FDpyaudLxK9XVUsmj8",
+  authDomain: "pips-official.firebaseapp.com",
+  projectId: "pips-official",
+  storageBucket: "pips-official.appspot.com",
+  messagingSenderId: "20388672074",
+  appId: "1:20388672074:web:976bc047e46901cdcbacb3",
+  measurementId: "G-SG28KW0EBN"
+};
 
-  /*messaging.onMessage((payload) => {
-  console.log('Message received. ', payload);*/
-  messaging.onBackgroundMessage(function(payload) {
-    console.log('Received background message ', payload);
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-    };
+/*messaging.onMessage((payload) => {
+console.log('Message received. ', payload);*/
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
 
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
-  });
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
