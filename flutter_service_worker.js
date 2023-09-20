0f2dd539778749f253d6c901af0231f2a1ab0768
mlab817 <mlab817@gmail.com>
2023-09-20 09:36:19 +0000

    Update from https://github.com/mlab817/xpips/commit/9b0fc1d19534adac09cf9627928b0b9e47c106af

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -60,7 +60,7 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b
 "assets/packages/elegant_notification/assets/icons/error.png": "2a84d22ca4a8d9123f1e3149121b0976",
 "assets/packages/elegant_notification/assets/icons/info.png": "84b36b60ddacca6c063112fd636bdefb",
 "assets/packages/elegant_notification/assets/icons/success.png": "a27784120d6634f48b24e12c4604f9d9",
-"assets/fonts/MaterialIcons-Regular.otf": "94974ec0d50a67ecaf16352f3fa2b9a8",
+"assets/fonts/MaterialIcons-Regular.otf": "08744f03fd45257d6deb4485fa3238a2",
 "assets/FontManifest.json": "efdfc00013675bf6810e20c693122f3b",
 "version.json": "868449ac59434bbbeae39692a24af023",
 "icons/Icon-maskable-192.png": "49a3968ca0bdc781ffc0159228853af3",
@@ -68,11 +68,11 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b
 "icons/Icon-maskable-512.png": "cee92d9031357ff9bfcf83001d503512",
 "icons/Icon-512.png": "cee92d9031357ff9bfcf83001d503512",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"index.html": "d1554fd154110a075a4ed422f4304bcd",
-"/": "d1554fd154110a075a4ed422f4304bcd",
+"index.html": "5222ad628421b4d15ae533d542dd3b04",
+"/": "5222ad628421b4d15ae533d542dd3b04",
 "favicon.png": "2428af5f724dd3374de3718ba29f45a2",
 "firebase-messaging-sw.js": "1795bbcc857fb6062f37841341fa0c71",
-"main.dart.js": "d8f35c426df360275d6beec9bd1297bf",
+"main.dart.js": "46c1985079cf4a6cb753d5e67e8f85dc",
 "manifest.json": "7b60c7c7866f307aae5d29ae04feef6b",
 "README.md": "82610568180029b050b8e6923035fea7"};
 // The application shell files that are downloaded before a service worker can
