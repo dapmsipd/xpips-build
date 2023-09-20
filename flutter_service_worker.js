00a4a78aa8616e67e88ddb7048b4c6607abeccbd
mlab817 <mlab817@gmail.com>
2023-09-20 06:59:56 +0000

    Update from https://github.com/mlab817/xpips/commit/047515fce0298d63094979572a9eb647d433328c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -68,11 +68,11 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b
 "icons/Icon-maskable-512.png": "cee92d9031357ff9bfcf83001d503512",
 "icons/Icon-512.png": "cee92d9031357ff9bfcf83001d503512",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"index.html": "2341306ea6abe789f6c78a7b80a8466c",
-"/": "2341306ea6abe789f6c78a7b80a8466c",
+"index.html": "d1554fd154110a075a4ed422f4304bcd",
+"/": "d1554fd154110a075a4ed422f4304bcd",
 "favicon.png": "2428af5f724dd3374de3718ba29f45a2",
 "firebase-messaging-sw.js": "1795bbcc857fb6062f37841341fa0c71",
-"main.dart.js": "1fbc8ccce8a5d33bc7f8b09770cf1dc9",
+"main.dart.js": "d8f35c426df360275d6beec9bd1297bf",
 "manifest.json": "7b60c7c7866f307aae5d29ae04feef6b",
 "README.md": "82610568180029b050b8e6923035fea7"};
 // The application shell files that are downloaded before a service worker can
